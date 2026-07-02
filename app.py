5f9db7f841bde786cbe69d23a18331ce99d7f743
ARNAV MALHOTRA [ TTL <a0002282.ttl@tatamotors.com>
2026-07-02 12:06:06 +0530

    modified search query function

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI, HTTPException, status, Request
 from fastapi.responses import Response, StreamingResponse, JSONResponse, HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
+from amzn import ping_amazon
+from get_product_info import amzn_product_info_scraper
+from app_logging import logger
 import uvicorn
 import psycopg2
+import asyncio
 import os
 from dotenv import load_dotenv
+import re
+
+
+def is_url(string):
+    pattern = r'^(http|https)'
+    return bool(re.match(pattern, string))
 
 load_dotenv()
 
@@ -29,8 +39,128 @@ app.add_middleware(
 @app.get("/api/search_query")
 def search_query(q: str):
     print(f"Query received on fastapi server: {q}")
+    log.info("Fastapi server received this query from nextjs: ", q)
+    if not q:
+        log.error(f"Received none as search query: {q}")
+        return {
+            "status": "error",
+            "details": f"Received none as search query, {q}"
+        }
+    if is_url(q):
+        print("Recieved an url")
+        log.info(f"Received an url: {q}")
+        url = q
+    else:
+        print("Received text")
+        log.info(f"Received text: {q}")
+        country_code = "in"
+        url =  f"https://www.amazon.{country_code}/s?k={q.replace(' ', '+')}"
 
-    return {
-        "status": "success",
-        "query_received": q
-    }
+    html_content = asyncio.run(ping_amazon(url))
+    check_url = re.search("/dp/(.*?)/", url) # check if url is product or search 
+
+    conn = psycopg2.connect(**CONFIG)
+
+    # Product url
+    if check_url:
+        print("[INFO] Product url detected")
+        log.info("Product url detected")
+        # match = re.search(r'/dp/([A-Z0-9]{10})', url)
+        # asin =  match.group(1) if match else None
+        product_info = amzn_product_info_scraper(html_content, url)
+
+        if len(product_info)==0:
+            log.error(f"{product_info.get("asin")}: Product information was not fetched due to captcha page or other reason, unable to update.")
+            print("[ERROR] Product information was not fetched (or page was blocked)")
+            conn.close()
+            return {
+                "status": "error",
+                "details": f"{product_info.get("asin")}: Unable to fetch product details. Could be due to captcha or other reason."
+            }
+
+        else:
+            save_to_database(product_info, conn) 
+            conn.close()
+            return {
+                "status": "success",
+                "details": f"Received product details for: {product_info.get("asin")}",
+                "content-type": type(product_info)
+                "content": product_info
+            }
+            
+        
+    # Search url
+    else:
+        print("[INFO] Search url detected") 
+        print("[INFO] Starting product search")
+        log.info("Search url detected")
+        log.info("Starting product search")
+        
+        max_pages = 5
+        current_page = 1
+
+        current_url = url
+        all_products = []
+
+        while current_url and current_page<=max_pages:
+            print(f"[INFO] Scraping search page {current_page}/{max_pages}: {current_url}")
+            log.info(f"Scraping search page {current_page}/{max_pages}: {current_url}")
+
+            if current_page!=1:
+                html_content = asyncio.run(ping_amazon(current_url))
+           
+
+            if not html_content or not html_content.content:
+                print("[ERROR] Received empty html or failed to fetch search page")
+                log.error("Received empty html or failed to fetch search page")
+                break
+
+            country_code = extract_country_code(current_url)
+            base_url = f"https://www.amazon.{country_code}"
+
+            products = get_search_results(html_content, base_url, country_code)
+
+            # Check if we got valid results
+            if not products:
+                print(f"[ERROR] No products found on page {current_page} (or page was blocked)")
+                log.error(f"No products found on page {current_page} (or page was blocked)")
+                break
+
+            print(f"[INFO] Found {len(products)} products on page {current_page}")
+            log.info(f"Found {len(products)} products on page {current_page}")
+            all_products.extend(products)
+
+            # Stop if we've reached the requested number of pages
+            if current_page >= max_pages:
+                break
+                
+            # Get URL for the next page
+            next_url = parse_pagination_url(html_content, base_url)
+            if not next_url:
+                print("No next page found. End of results.")
+                break
+                
+            current_url = next_url
+            current_page += 1
+            
+        print(f"\nSearch completed. Total products found: {len(all_products)}\n")
+        log.info(f"Search completed. Total products found: {len(all_products)}\n")
+
+        if len(all_products) == 0:
+            return {
+                "status": "error",
+                "details": f"{url}: Could not find any products on search page. Could be due to captcha or other reason."
+            }
+        
+        save_to_database(all_products, conn)
+        conn.close() 
+        return {
+            "status": "succes",
+            "details": f"Found {len(all_products)} products from search pages",
+            "content-type": type(all_products),
+            "content": all_products
+        } 
+        
+    
+
+
--- test.py
+++ test.py
@@ -30,8 +30,16 @@ CURRENCY_MAPPING = {
 
 # print(CURRENCY_MAPPING.get("₹"))
 
-with open("C:\\Users\\Arnav's Lappy\\Downloads\\tracker-logs-28364454346\\B0GY1J37HT_debug_response.html", 'r', encoding='utf-8') as file:
-    html_content = file.read()
+# with open("C:\\Users\\Arnav's Lappy\\Downloads\\tracker-logs-28364454346\\B0GY1J37HT_debug_response.html", 'r', encoding='utf-8') as file:
+#     html_content = file.read()
 
-if "captcha" in html_content.lower() or "api-services-support@amazon.com" in html_content.lower():
-    print("CAPTCHA or anti-bot measure detected in response")
+# if "captcha" in html_content.lower() or "api-services-support@amazon.com" in html_content.lower():
+#     print("CAPTCHA or anti-bot measure detected in response")
+
+import re
+def is_url(string):
+    pattern = r'^(http|https)'
+    return bool(re.match(pattern, string))
+# Example usage
+print(is_url("htt://www.amazon.in/s?k=ps5"))  # True
+print(is_url("not_a_url"))   
